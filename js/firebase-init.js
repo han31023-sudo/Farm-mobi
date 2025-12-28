c8f7faf55c94e2d0cf8aedd1f13e6a46a24a225b
han31023-sudo <han31023@gmail.com>
2025-12-29 02:41:17 +0700

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -3,8 +3,8 @@ const firebaseConfig = {
   authDomain: "PROJECT.firebaseapp.com",
   projectId: "PROJECT",
   storageBucket: "PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  messagingSenderId: "AIzaSyAiR28Tc3LtCrR8ysUYgiSGRfWSNG7d5pk",
+  appId: "1:552887299452:android:dd941741f62197b37388f9"
 };
 
 firebase.initializeApp(firebaseConfig);
