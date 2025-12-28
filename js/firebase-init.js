1b03d5414182b451aa9c2be868cd60018c25b266
han31023-sudo <han31023@gmail.com>
2025-12-29 02:16:29 +0700

    Create firebase-init.js

---
+++ js/firebase-init.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "API_KEY_KAMU",
+  authDomain: "PROJECT.firebaseapp.com",
+  projectId: "PROJECT",
+  storageBucket: "PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+firebase.initializeApp(firebaseConfig);
